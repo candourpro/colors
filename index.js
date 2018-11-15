f02e33a4be35620f59e664319553526538657c30
Domas <domas.bitvinskas@me.com>
2018-11-15 01:12:50 +0000

    v1.1.0: support svg fill

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 
 export default {
   match: ({ colors }, value, key) => (
-    colors && value && key.match(/color/i) && _.has(colors, value)
+    colors && value && key.match(/color|fill/i) && _.has(colors, value)
   ),
   value: ({ colors }, value) => colors[value],
 }
